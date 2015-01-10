71dd80079fc2173686013bd10a1aca5dc54542df
符现超 <fuxianchao@ITsupportdeMacBook-Pro.local>
2015-01-10 20:13:04 +0800

    Initial Commit

---
+++ LoopView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoopView
+//
+//  Created by 符现超 on 15/1/10.
+//  Copyright (c) 2015年 当当. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoopView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoopView
+//
+//  Created by 符现超 on 15/1/10.
+//  Copyright (c) 2015年 当当. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
